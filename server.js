00bc661b469c372bc0a31fac8cc7b06513009438
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-09-08 09:09:30 +0200

    test port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 var corsOptions = {
-  origin: "https://jesse-96e14.web.app", 
+  origin: "http://localhost:8080", 
   credentials: true,
 };
 
