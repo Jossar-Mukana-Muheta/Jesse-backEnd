c459e26664f85030a9afa52dd5c6bc7d6b00184d
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-09-08 02:11:12 +0200

    ok

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
 });
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
